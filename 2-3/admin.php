7f203bf7d4cf543f7dfb124807dd8d279bf30604
GREEV\greev <810411@mail.ru>
2018-05-20 02:26:35 +0500

    выполнено

---
+++ 2-3/admin.php
@@ -0,0 +1,41 @@
+<?php
+$info = '';
+
+$uploads_dir = __DIR__ . '/uploads';
+if (!file_exists($uploads_dir)) mkdir($uploads_dir);
+
+if ($_FILES) {
+    $name = basename($_FILES['userfile']['name']);
+    $tmp_name = $_FILES['userfile']['tmp_name'];
+    $path_info = pathinfo($name);
+
+    if ($_FILES['userfile']['error'] == UPLOAD_ERR_OK && $_FILES['userfile']['type'] == 'text/plain' && $path_info['extension'] === 'json') {
+        move_uploaded_file($tmp_name, "$uploads_dir/$name");
+        header("Location: list.php");
+        //$info = "Файл загружен";
+    } else {
+        $info = "Ошибка загрузки файла";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Задание к лекции "Обработка форм"</title>
+    <meta name="description" content="Нетология. Курс PHP/SQL. Урок 2.2">
+</head>
+<body>
+<ul>
+    <li><a href="list.php">Список загруженных тестов</a></li>
+</ul>
+<h3>Загрузка тестов</h3>
+<form enctype="multipart/form-data" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000"/>
+    Выберите файл с тестами в формате JSON: <br>
+    <input name="userfile" type="file"/><br>
+    <input type="submit" value="Загрузить"/><br>
+    <?php echo $info ?>
+</form>
+</body>
+</html>
---
+++ 2-3/test.php
@@ -0,0 +1,103 @@
+<?php
+$file_list = glob(__DIR__ . '/uploads/*.json');
+$test = [];
+$result = 0;
+$info = '';
+
+foreach ($file_list as $key => $file) {
+    if ($key == @$_GET['test']) {
+        $test = json_decode(file_get_contents($file_list[$key]), true);
+        $test_name = pathinfo($file)['filename'];
+    }
+}
+
+if (!isset($test[0]['q'])) {
+    http_response_code(404);
+    echo '404. Not Found <br>';
+    echo 'В файле ' . $test_name . ' тестов не найдено';
+    echo '<br><a href="list.php">Список загруженных тестов</a>';
+    exit(1);
+}
+
+foreach ($test as $key => $value) {
+    $questions[] = $value['q'];
+    $answers[] = $value['ans'];
+}
+
+if (!empty($_POST)) {
+    $user = $_POST['user'];
+    $userAnswers = array_slice($_POST, 0, -4);
+    foreach ($userAnswers as $key => $value) {
+        foreach ($answers[$key] as $answer) {
+            if ($value == $answer['a']) {
+                if ($answer['r']) {
+                    $result++;
+                }
+            }
+        }
+    }
+    if ($result > 0) {
+        $texts[] = 'от ' . date("d-m-Y");
+        $texts[] = 'удостоверяет, что';
+        $texts[] = $_POST['user'];
+        $texts[] = 'прошел ' . $test_name;
+        $texts[] = 'ответив вопросов ' . $result . ' из ' . count($questions);
+
+        $path = __DIR__ . '/images/sert.png';
+        $img = imagecreatefrompng($path);
+        $font = __DIR__ . '/images/SegoeUI.ttf';
+        $black = imagecolorallocate($img, 0, 0, 0);
+        $imageSize = getimagesize($path);
+        $y = $imageSize[1] / 3;
+
+        foreach ($texts as $text) {
+            $textPoints = imagettfbbox(20, 0, $font, $text);
+            $pos = ($imageSize[0] - $textPoints[2] - $textPoints[0]) / 2;
+            imagettftext($img, 20, 0, $pos, $y += 40, $black, $font, $text);
+        }
+        imagettftext($img, 20, 0, $imageSize[0] / 2 + $imageSize[0] / 4,
+            $imageSize[1] - $imageSize[1] / 5, $black, $font, "В.В.");
+
+        header('Content-type: image/png');
+        imagepng($img);
+        imagedestroy($img);
+
+    } else if (!empty($_POST)) {
+        $info = $_POST['user'] . ', вы ответили неправильно';
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Задание к лекции "Обработка форм"</title>
+    <meta name="description" content="Нетология. Курс PHP/SQL. Урок 2.2">
+</head>
+<body>
+<ul>
+    <li><a href="admin.php">Загрузчик тестов</a></li>
+    <li><a href="list.php">Список загруженных тестов</a></li>
+</ul>
+<h3><?= $test_name ?></h3>
+<form action="" method="POST">
+    <?php for ($i = 0; $i < count($questions); $i++) : ?>
+        <fieldset>
+            <legend><?= $questions[$i]; ?></legend>
+            <?php foreach ($answers[$i] as $a => $answer) : ?>
+                <label><input type="radio" name="<?= $i; ?>"
+                              value="<?= $answer['a']; ?>"> <?= $answer['a']; ?>
+                </label>
+            <?php endforeach; ?>
+        </fieldset>
+    <?php endfor; ?><br>
+    <input type="text" name="user" placeholder="Введите ваше имя"><br><br>
+    <input type="hidden" name="title" value=<?= $test_name ?>>
+    <input type="hidden" name="result" value=<?= $result ?>>
+    <input type="hidden" name="sum" value=<?= count($questions) ?>>
+    <input type="submit" value="Отправить">
+</form>
+<br>
+<?php echo $info; ?>
+</body>
+</html>
